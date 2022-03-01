16f0d952224772f5d805de44a377e61427c741d3
nathalia <nathaliatroina@gmail.com>
2022-03-01 14:09:31 -0500

    commit tree 741671c25f589ca1eaa86c4b84f0f16175910247

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,63 @@
 class Movie < ActiveRecord::Base
+    #base herda da class
+#talking about movie table
+#self means that it is talking about the movie itself
+#quando there is no self significa que e an instance
+#quando tem '.' is a class, with self 
+# quando "#" no self
+
+#CREATE
+
+def self.create_with_title(title)
+    Movie.create(title: title)
+  end
+
+#READ
+
+def self.first_movie
+    Movie.first
+end
+
+def self.last_movie
+    Movie.last
+end
+
+def self.movie_count
+    Movie.count
+end
+
+def self.find_movie_with_id(id)
+    Movie.find(id)
+end
+
+def self.find_movie_with_attributes(attributes)
+    Movie.find_by(attributes)
+end
+
+def  self.find_movies_after_2002
+    Movie.where("release_date > 2002")
+end
+
+#UPDATE
+
+def update_with_attributes(attributes)
+    self.update(attributes)
+end
+
+def self.update_all_titles(title)
+    Movie.update(title: title)
+  end
+
+#DELETE  
+
+  def self.delete_by_id(id)
+    #vai self pq diz que e single
+    movie = Movie.find(id)
+    movie.destroy
+  end
+
+  def self.delete_all_movies
+    Movie.destroy_all
+  end
 
 end
